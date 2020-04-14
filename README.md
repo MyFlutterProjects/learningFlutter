f79c3a817f5065c975ce9f124ccd0ff663a19ab6
kisakyamukama <you@efitzerkisakyamukama03@gmail.com>
2020-04-14 07:45:30 +0300

    add basic widgets

--- README.md
+++ README.md
@@ -12,6 +12,15 @@ Am following the official flutter docs
     - runApp() takes a widget and makes it the root of the tree
     
 ### Basic Widgets
+    - Text: lets us run styled text withi  our application
+    - Row, Column: create flexible layouts: horizontal (Row) and vertical(Column)
+    - Stack: used to place widgets on top of each other, in paint order.
+    - Container: lets us create rectangular visual elements. This can have BoxDecoration, background, border or a shadoe, can be also transformed into 3D
+
+    - **  material is a conceptual piece of paper on which the UI appears
+
+    - Expanded: fillup all remaining space,multiple can be used with ***flex*** to determine ratio.
+
 ### Using Material component
 ### Handling gestures
 ### Changing Widget in response to input
--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,74 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp( 
-    Center( 
-      child: Text('Hello, world!', 
-      textDirection: TextDirection.ltr,
-      ),
-      
-    ),
+   MaterialApp( 
+     title: 'My app',
+     debugShowCheckedModeBanner: false,
+     home: MyScaffold(),
+   ),
   );
+}
+
+class MyScaffold extends StatelessWidget { 
+  @override 
+  Widget build(BuildContext context) { 
+    return Material(  //material is a conceptual piece of paper on which the UI appears
+      child: Column( 
+        children: <Widget>[ 
+          MyAppBar( 
+            title: Text( 
+              'Example title',
+              style: Theme.of(context).primaryTextTheme.title,
+            ),
+          ),
+          Expanded( 
+            child: Center( 
+              child: Text('Hello, world'),
+            ),
+          )
+        ], 
+        ),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget { 
+  MyAppBar({this.title});
+
+  // fiels in a widget subclass are always marked final
+  final Widget title; 
+
+  @override 
+  Widget build(BuildContext context) { 
+    return Container( 
+      height:96.0, // in logical pixels
+      // padding on the left and right 
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.pink[500]),
+
+      child: Row( 
+        children: <Widget>[
+          IconButton( 
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: (){},
+          ),
+          // expanded fill all aviable space
+          Expanded( 
+            child: title,
+          ),
+          IconButton( 
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          )
+
+
+        ],
+        ),
+
+
+    );
+  }
 }
 
